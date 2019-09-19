3ee1d64b8b6dfd0744f698f12fce40d054feb362
anthonykao10 <anthonykao10@gmail.com>
2019-09-19 03:16:13 +0000

    Update query using parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,55 +6,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-/* Query db */
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
 
-// pool.query(`
-// SELECT students.id, students.name, cohorts.name AS cohort
-// FROM students
-// JOIN cohorts ON cohorts.id = cohort_id
-// LIMIT 5;
-// `)
-// .then(res => {
-//   console.log(res);
-//   console.log(typeof res);
-//   console.log(res.rows[0]);
-// })
-// .catch(err => console.error('query error', err.stack));
-
-
-/* Query for cohorts.name instead of just cohort_id */
-
-// pool.query(`
-// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-// FROM students
-// JOIN cohorts ON cohorts.id = cohort_id
-// LIMIT 5;
-// `)
-// .then(res => {
-//   res.rows.forEach(user => {
-//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-//   })
-// });
-
-
-/* Query cohort name and limit dynamically */
-// const [ , , cohortName, resultLimit ] = process.argv;
-
-// if (!cohortName || !resultLimit) {
-//   pool.end();
-//   return;
-// } 
-
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+pool.query(queryString, values)
+  .then(res => {
+    console.log(res);
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+    pool.end();
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
