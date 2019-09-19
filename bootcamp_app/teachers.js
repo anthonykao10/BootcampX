17b9a63dc955e7df9da0cc9b93250ee14ee6fe1c
anthonykao10 <anthonykao10@gmail.com>
2019-09-19 03:16:28 +0000

    Update teachers query using parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,19 +6,26 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+
 /* Get all teachers that made an assistance request during a cohort. */
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-FROM assistance_requests
-  JOIN teachers ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teachers.name;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  });
-})
-.catch(err => console.error('query error', err.stack));
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM assistance_requests
+    JOIN teachers ON teachers.id = teacher_id
+    JOIN students ON students.id = student_id
+    JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = $1
+  ORDER BY teachers.name;
+`;
+const cohortName = process.argv[2];
+const values = [`${cohortName}`];
+
+pool.query(queryString, values)
+  .then(res => {
+    console.log(res);
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+    pool.end();
+  })
+  .catch(err => console.error('query error', err.stack));
